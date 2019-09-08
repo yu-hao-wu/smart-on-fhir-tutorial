12735350c93a96d69b4482cf6bd95d860a6c25f8
Yuhao <ggglowu@gmail.com>
2019-09-08 16:55:06 +0100

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -13,7 +13,7 @@
        // console.log('people are:', people);                             
         var patient = smart.user;
         var pt = patient.read();
-        smart.api.search({type: "Patient", query: {given: ["John", "Bob"], family: "Smith"}})
+        smart.api.search({type: "Patient", query: {family: "Smith"}})
         .then(function(bundle){
         console.log('Search patients', bundle)
         });
